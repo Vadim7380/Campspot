26ad588579c02b70065823131d05442c6bcc3f83
Vadim7380 <prikhodkovn@gmail.com>
2024-01-02 13:59:35 -0700

    0.00024703557312253

---
+++ cypress/e2e/Entry_Page/HomePage.cy.js
@@ -0,0 +1,83 @@
+import HomePagePO from './HomePagePO.cy.js';
+
+describe ('Home Page', ()=>{
+   
+    const page = new HomePagePO();
+
+    beforeEach('Page Open', ()=>{
+
+        cy.visit('https://www.campspot.com/');
+    })
+
+    it ('list view should contain correct element', ()=>{
+
+      //icons and links on nav bar
+      page.dashboardButton().should('be.visible');
+      page.rentAnRV().should('be.visible');
+      page.signIn().should('be.visible');
+      page.createAccount().should('be.visible');
+      page.shoppingCart().should('be.visible');
+
+      //find your campspot searching
+      page.searchHeading().should('be.visible'); 
+      page.searchDescription().should('be.visible');
+      page.searchDescription().should('contain', 'Discover and book the best campgrounds');
+      page.locationLabel().should('be.visible');
+      page.locationValue().should('be.visible');
+      page.datesLabel().should('be.visible');
+      page.checkInDate().should('be.visible');
+      page.checkOutDate().should('be.visible');
+      page.guestsLabel().should('be.visible');
+      page.guestsValue().should('be.visible');
+      page.searchButton().should('be.visible');
+
+      //home why content
+      page.whyCampspotTitle().should('be.visible');
+      page.favoriteCampgroundsTitle().should('be.visible');
+      page.favoriteCampgroundsTitle().should('contain', ' Your favorite campgrounds');
+      page.favoriteCampgroundsDescription().should('be.visible');
+      page.favoriteCampgroundsDescription().should('contain', 'Campspot lists top-rated camping destinations available for online booking in North America.');
+      page.instantBookingTitle().should('be.visible');
+      page.instantBookingDescription().should('be.visible');
+      page.instantBookingDescription().should('contain', 'No need to call the campground or wait for your booking to be accepted.');
+      page.noMembershipFeeTitle().should('be.visible');
+      page.noMembershipFeeDescription().should('be.visible');
+      page.noMembershipFeeDescription().should('contain', 'Book all listed campgrounds on our app instantly');
+
+      //way to stay
+      page.wayToStayTitle().should('be.visible');
+      page.cabinsLodging().should('be.visible');
+      page.cabinsLodgingDescription().should('be.visible');
+      page.cabinsLodgingDescription().should('contain', 'Find comfort and convenience with a cabin or lodge'); 
+      page.cabinsLodgingImage().should('be.visible')
+
+      //alternative option to validate image
+      //.and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        // expect($img[0].naturalWidth).to.be.eq(670)
+        // expect($img[0].naturalHeight).to.be.eq(447)
+      //})
+      page.rvSites().should('be.visible');
+      page.rvSitesDescription().should('be.visible');
+      page.rvSitesDescription().should('contain', 'Camp in style from the comfort of your vehicle'); 
+      page.rvSitesImage().should('be.visible') 
+      page.tentSites().should('be.visible');
+      page.tentSitesDescription().should('be.visible');
+      page.tentSitesDescription().should('contain', 'Pitch your tent and relax next to the fire'); 
+      page.tentSitesImage().should('be.visible');
+      
+      //ads content
+      page.adContentLink().should('have.attr', 'href').and('include', 'https://clicks-partners.rvshare.com');
+      page.adContentImage().should('be.visible');
+
+
+      
+
+
+
+
+
+
+    })
+
+})
---
+++ cypress/e2e/Entry_Page/HomePagePO.cy.js
@@ -0,0 +1,51 @@
+class HomePagePO {
+
+
+    //icons and links on nav bar
+    dashboardButton() { return cy.get('.mod-white');}
+    rentAnRV() { return cy.get('.header-nav-link.mod-rent-an-rv.mod-home');}
+    signIn() { return cy.get('.app-sign-in-nav-link');}
+    createAccount() { return cy.get('.mod-create-account');}
+    shoppingCart() { return cy.get('.header-cart-icon-image.mod-home');}
+
+    //find your campspot searching
+    searchHeading() { return cy.get('.home-hero-search-heading');}
+    searchDescription() { return cy.get('.home-hero-search-description');}
+    locationLabel() { return cy.get('.home-hero-search-form-location > .home-hero-search-form-label');}
+    locationValue() { return cy.get('.location-search-input');}
+    datesLabel() { return cy.get('.home-hero-search-form-dates > .home-hero-search-form-label');}
+    checkInDate() { return cy.get('.aggredator-field-input.app-aggredator-checkin-date.mod-placeholder');}
+    checkOutDate() { return cy.get('.aggredator-field-input.app-aggredator-checkout-date.mod-placeholder');}
+    guestsLabel() { return cy.get('.home-hero-search-form-guests > .home-hero-search-form-label');}
+    guestsValue() { return cy.get('.guests-picker-input-text');}
+    searchButton() { return cy.get('.home-hero-search-form-submit-button');}
+
+    //home why content
+    whyCampspotTitle() { return cy.get('.home-why-title');}
+    favoriteCampgroundsTitle() { return cy.get('li:nth-of-type(1) > .home-why-list-item-reason');}
+    favoriteCampgroundsDescription() { return cy.get('li:nth-of-type(1) > .home-why-list-item-description');}
+    instantBookingTitle() { return cy.get(':nth-child(2) > .home-why-list-item-reason');}
+    instantBookingDescription() { return cy.get(':nth-child(2) > .home-why-list-item-description');}
+    noMembershipFeeTitle()  { return cy.get("section[class='home-why'] li:nth-child(3) h3:nth-child(1)");}
+    noMembershipFeeDescription()  { return cy.get("body > app-root:nth-child(2) > div:nth-child(1) > main:nth-child(2) > search:nth-child(2) > main:nth-child(1) > section:nth-child(3) > div:nth-child(1) > ul:nth-child(2) > li:nth-child(3) > p:nth-child(2)");}
+
+    //way to stay
+    wayToStayTitle() { return cy.get('.home-categories-title');}
+    cabinsLodging() { return cy.get(':nth-child(1) > .home-categories-list-item-tile > .home-categories-list-item-content > .home-categories-list-item-name');}
+    cabinsLodgingDescription() { return cy.get(':nth-child(1) > .home-categories-list-item-tile > .home-categories-list-item-content > .home-categories-list-item-description');}
+    cabinsLodgingImage() { return cy.get('img[src="/assets/img/home-ways-to-stay-lodging-cava-robles-670x447.jpg"]');}
+    rvSites() { return cy.get('.app-rv-sites-category-button.home-categories-list-item-tile > .home-categories-list-item-content > .home-categories-list-item-name');}
+    rvSitesDescription() { return cy.get('.app-rv-sites-category-button.home-categories-list-item-tile > .home-categories-list-item-content > .home-categories-list-item-description');}
+    rvSitesImage() { return cy.get('img[src="/assets/img/home-ways-to-stay-rv-sites-elkamp-670x447.jpg"]');}
+    tentSites() { return cy.get('.app-tent-category-button.home-categories-list-item-tile > .home-categories-list-item-content > .home-categories-list-item-name');}
+    tentSitesDescription() { return cy.get('.app-tent-category-button.home-categories-list-item-tile > .home-categories-list-item-content > .home-categories-list-item-description');}
+    tentSitesImage() { return cy.get('img[src="/assets/img/home-ways-to-stay-tent-sites.jpg"]');}
+
+    //ads content
+    adContentLink() { return cy.get('.home-ad-click');}
+    adContentImage() { return cy.get('img[src="/assets/img/rvshare-1170x250.jpg"]');}
+
+
+}
+
+export default HomePagePO
